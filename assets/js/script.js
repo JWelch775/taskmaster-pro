29dcceace224a8e4064e374fe6f3a4d881aa6831
Justin Welch <justin.welch075@gmail.com>
2021-01-19 14:23:19 -0600

    added task persistence with dragging and dropping in different columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
-  var taskLi = $("<li>").addClass("list-group-item");
+  var taskLi = $("<li>").addClass("list-group-item border border-dark");
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
@@ -175,6 +175,63 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+
+//drag and drop
+$(".card .list-group").sortable(
+  {
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event)
+      {
+        //console.log("activate", this);
+      },
+    deactivate: function(event)
+      {
+        //console.log("deactivate", this);
+      },
+    over: function(event)
+      {
+        //console.log("over", event.target);
+      },
+    out: function(event)
+      {
+       // console.log("out", event.target);
+      },
+    update: function(event)
+      {
+        var tempArr = []
+        //loop over current set of children in sortable list
+        $(this).children().each(function()
+        {
+          var text = $(this)
+            .find("p")
+            .text()
+            .trim();
+
+          var date = $(this)
+            .find("span")
+            .text()
+            .trim();
+
+          //add task data to the temp array as an object
+          tempArr.push(
+            {
+              text: text,
+              date: date
+            });
+        });    
+        //trim down list's ID to match object property
+        var arrName = $(this)
+          .attr("id")
+          .replace("list-", "")
+
+        //update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+      }
+  });
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
